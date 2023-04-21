5e48d5f7ce88784fef454f9936805d56b184f7fc
Guille <sokerwild96@gmail.com>
2023-04-21 17:02:12 +0200

    Seccion 6 acabada

---
+++ 06/06-01-Arrays.js
@@ -0,0 +1,12 @@
+// !DEPRECATED
+// let cars = new Array('Audi', 'BMW', 'Seat');
+
+// ------------- //
+
+const cars = ['Audi', 'BMW', 'Seat'];
+
+cars.push('Volvo');
+
+console.log(typeof cars);
+console.log(Array.isArray(cars));
+console.log(cars instanceof Array);
