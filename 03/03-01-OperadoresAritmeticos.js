184238ed5facf6127ae30c032ca498423d91bd5f
Guille <sokerwild96@gmail.com>
2023-04-21 17:01:40 +0200

    Seccion 3 acabada

---
+++ 03/03-01-OperadoresAritmeticos.js
@@ -0,0 +1,43 @@
+let a = 3, b = 2;
+
+// Suma
+let z = a + b;
+
+// Resta
+z = a - b;
+
+// Division
+z = a / b;
+
+// Multiplicacion
+z = a * b;
+
+// Resto
+z = a % b;
+
+// Elevado a x
+z = a ** b;
+console.log(z);
+
+// 20.Video
+
+let c = 1;
+z = 0;
+
+console.log(a + b - c)
+
+// Operador ternario
+
+let resultado = 3 > 2 ? true : false;
+
+console.log(resultado);
+
+// Cast String a Int
+let num = "10";
+let age = Number(num);
+console.log(typeof age);
+
+if(!isNaN(age))
+{
+    console.log(age);
+}
